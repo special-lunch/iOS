6187ce339bcd30982df650236b7ce33e853716aa
Joons <elddy0948@gmail.com>
2022-08-02 21:51:41 +0900

    project setup RxSwift, remove unnecessary commits

--- SpecialLunch/SpecialLunch/ViewController.swift
+++ SpecialLunch/SpecialLunch/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  SpecialLunch
-//
-//  Created by 김호준 on 2022/08/02.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemRed
   }
-
-
 }
 
